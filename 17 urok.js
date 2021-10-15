0f0f110d80a636c149eef5e5ea4ee166bbd4b793
Zori <list_babayan@mail.ru>
2021-10-15 22:53:18 +0400

    git

---
+++ 17 urok.js
@@ -0,0 +1,21 @@
+
+
+const ptp = 'ZORI';
+
+console.log(ptp.toLocaleLowerCase()); // превращает строку в нижний регистр
+console.log(ptp.toUpperCase());       // превращает строку в верхний регистр
+
+   const Arab = 'Trap Rap'
+   console.log(Arab.indexOf('Rap'));    //поиск код строки 
+
+
+   const low = "Helo World";
+   console.log(low.slice(3, 9)); //3 начало строки 9 конец строки того чего нам необходимо 
+
+
+   const num = 16.36958;
+   console.log(Math.round(num)); //округление числа до ближайшего челого 
+
+   const test = "13.7px";
+   console.log(parseInt(test));//перевод числа в другое систему исчисления  "13"
+   console.log(parseFloat(test));// возврашения значения с плавающей точкой  "13.7" 
