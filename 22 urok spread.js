af1968e4756fbaf505b42704657e5b83f70a29c4
Zori <list_babayan@mail.ru>
2021-10-17 23:06:09 +0400

    spreaad операторы, поверхносное копирование

---
+++ 22 urok spread.js
@@ -0,0 +1,11 @@
+const socialNetwork = ['vk', 'fb', 'insta', 'ok'],
+      sremservic = ['ivi', 'netflix', 'megogo'],
+
+      all=[...socialNetwork, ...sremservic, 'ozon', 'aliexpress'];
+
+      console.log(all);
+
+
+      const array = ['a', 'v'];
+      const newArray = [...array, '2', '98'];
+      console.log(newArray);
